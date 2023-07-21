b5efb916bc67347226b1677a3643e77c081f1834
Sylvia T <sylviarosemarie@gmail.com>
2023-07-21 13:20:48 -0500

    started working on locations array in database

--- scripts/database.js
+++ scripts/database.js
@@ -51,4 +51,42 @@ const database = {
       food: "small crustaceans, insect larvae, and algae",
     },
   ],
+
+  locations: [
+    {
+      id: 1,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Clouds_over_Lake_Kutubu.jpg/264px-Clouds_over_Lake_Kutubu.jpg",
+      place-name: "Lake Kutubu of Papau New Guinea",
+      fish-found: "Melanie of family Melanotaeniidae",
+    }
+    ,{
+      id: 2,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Ampera_Bridge_at_Noon%2C_Palembang.jpg/200px-Ampera_Bridge_at_Noon%2C_Palembang.jpg",
+      place-name: "Musi River of Sumatra, Indonesia",
+      fish-found: "Gimli of family Osphronemidae",
+    }
+    ,{
+      id: 3,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Moulouya_Marocco.jpg/250px-Moulouya_Marocco.jpg",
+      place-name: "Moulouya River of Morocco"
+      fish-found: "Pagliacci of family Cobitoidea"
+    }
+    ,{
+      id: 4,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/COLLECTIE_TROPENMUSEUM_Samenvloeiing_van_de_Tembesirivier_en_de_Batang_Hari_in_het_district_Rawas_Sumatra_TMnr_60002829.jpg/250px-COLLECTIE_TROPENMUSEUM_Samenvloeiing_van_de_Tembesirivier_en_de_Batang_Hari_in_het_district_Rawas_Sumatra_TMnr_60002829.jpg",
+      place-name: "Batan Hari River of Sumatra, Indonesia"
+      fish-found: 
+    }
+    ,{
+      id: 5,
+      image:
+      place-name:
+      fish-found:
+    }
+  ]
 };
+
+export const getFishies = () => {
+  return database.fishies.map((fish) => ({ ...fish }));
+};
+
