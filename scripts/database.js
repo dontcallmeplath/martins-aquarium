acec2ab7cbbd081e6b1a073bb43c5e386ae0b7d2
Sylvia T <sylviarosemarie@gmail.com>
2023-07-21 10:02:55 -0500

    Finished fish array for database

---
+++ scripts/database.js
@@ -0,0 +1,54 @@
+const database = {
+  fishies: [
+    {
+      id: 1,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Aulonocara_hansbaenschi.jpg/175px-Aulonocara_hansbaenschi.jpg",
+      family: "Cichlidae",
+      length: "15 cm",
+      name: "Sunny",
+      found: "Lake Malawi near Tanzania",
+      food: "algae and biofilm",
+    },
+    {
+      id: 2,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Tigerbarbe_Puntius_tetrazona.jpg/175px-Tigerbarbe_Puntius_tetrazona.jpg",
+      family: "Cyprinidae",
+      length: "7.5 cm",
+      name: "Tigger",
+      found: "Indonesia",
+      food: "invertebrates and vegetation",
+    },
+    {
+      id: 3,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/2015-09-13-223-Tiger-loach.jpg/175px-2015-09-13-223-Tiger-loach.jpg",
+      family: "Cobitoidea",
+      length: "20 cm",
+      name: "Pagliacci",
+      found: "Morocco",
+      food: "invertebrates and vegetation",
+    },
+    {
+      id: 4,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Colisa_lalia.jpg/175px-Colisa_lalia.jpg",
+      family: "Osphronemidae",
+      length: "5cm",
+      name: "Gimli",
+      found: "Sumatra",
+      food: "small aquatic organisms and carrion",
+    },
+    {
+      id: 5,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Melanotaenia_lacustris_thomnight_001.jpg/175px-Melanotaenia_lacustris_thomnight_001.jpg",
+      family: "Melanotaeniidae",
+      length: "12 cm",
+      name: "Melanie",
+      found: "Lake Kutubu of Papau New Guinea",
+      food: "small crustaceans, insect larvae, and algae",
+    },
+  ],
+};
