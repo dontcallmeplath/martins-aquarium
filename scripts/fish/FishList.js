224c103f5ef6949c29036793baa08aca2c135da4
Sylvia T <sylviarosemarie@gmail.com>
2023-07-25 14:36:42 -0500

    Tinkered with styling to prevent overlap of elements; Made & hooked up filter function in fish.js/main.js

--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -1,17 +1,34 @@
 import { getFishies } from "../database.js";
+const fishes = getFishies();
+let filteredFishArray = [];
 
-export const FishList = () => {
-  const fishes = getFishies();
+export const filteredFishList = () => {
   let htmlString = "";
-  for (const fish of fishes) {
+  for (const fish of filteredFishArray) {
     htmlString += `<section class="fish-card">
             <img  class="fish__image image--card" src="${fish.image}" />
             <div class="fish__name">Name: ${fish.name}</div>
             <div class="fish__family">Family: ${fish.family}</div>
-            <div class="fish__length">Length: ${fish.length}</div>
+            <div class="fish__length">Length in cm: ${fish.length}</div>
             <div class="fish__location">Found: ${fish.found}</div>
             <div class="fish__diet">Eats: ${fish.food}</div>
         </section>`;
   }
   return htmlString;
+};
+
+export const filterHolyFish = () => {
+  for (const fish of fishes) {
+    if (fish.length % 5 === 0) {
+      filteredFishArray.push(fish);
+    } else if (fish.length % 3 === 0) {
+      filteredFishArray.unshift(fish);
+    }
+  }
+  for (const fish of fishes) {
+    if (fish.length % 5 !== 0 && fish.length % 3 !== 0) {
+      filteredFishArray.push(fish);
+    }
+  }
+  return filteredFishList(filteredFishArray);
 };
